58503cb2d6371044eb82b3cbb9e29ae274b52f5b
Leah Tillman <leahtillman@Leahs-MacBook-Air.local>
2023-09-29 16:29:25 -0500

    just playing around, mostly just notes

--- Develop/script.js
+++ Develop/script.js
@@ -7,10 +7,22 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   var numberOfCharacters = prompt("How many characters?")
   var includeNumbers = confirm("Do you want numbers?")
+  var includeLowercase = confirm("Do you want lowercase letters?")
+  var includeUppercase = confirm("Do you want uppercase letters?")
 
+  if (includeNumbers) {
+    // how to add random numbers here? need to create array of chars?
+  } else { null }
 
-
+  if (includeLowercase) {
+    //how to display lowercase letters?
+  } else { null }
 }
+/* var myNewPassword = //all logic done under function
+return myNewPassword 
+*/
+
+
 
 
 
@@ -28,6 +40,9 @@ function generatePassword() {
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
+  //need to create a function called generatePassword, inside has return password, create a variable for the logic
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
